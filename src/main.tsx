e83a7bf30474a3a71da52721e59b8fb5b1efc944
Shamil-Dev-lk <shamildeveloperlk@gmail.com>
2026-07-21 10:31:49 +0530

    Update src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
 import { AppRouter } from '@/router';
@@ -36,8 +36,6 @@ async function bootstrap() {
   } finally {
     setLoading(false);
   }
-
-  // Listen for auth state changes (TOKEN_REFRESHED, SIGNED_OUT, etc.)
   try {
     authService.onAuthStateChange((user) => setUser(user));
   } catch { /* ignore */ }
@@ -94,7 +92,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
+        <HashRouter>
           <AutoUpdatingApp />
           <Toaster
             position="top-right"
@@ -110,7 +108,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
               error: { iconTheme: { primary: '#CC0000', secondary: '#fff' } },
             }}
           />
-        </BrowserRouter>
+        </HashRouter>
       </QueryClientProvider>
     </ErrorBoundary>
   </React.StrictMode>
